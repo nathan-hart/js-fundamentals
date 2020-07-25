b5437099d5893d90dc249e21e7268b2a6655b08b
nathan <nathanjbro@gmail.com>
2020-07-25 22:13:35 +0300

    js and html

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello world')
+
+console.log(typeof undefined);
+
+// difference between undefined and null
+
+console.log(x) //undefined - it has no value assigned
+
+function x() {
+    return null // defined explicitly, represents empty value
+}
+
+// primitive data types- int null undefined string bool
+// object type - represents more complex data structures - arrays, objects, functions - can be mutated after being assigned a variable
+
